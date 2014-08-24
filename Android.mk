82cd63158b79ce6a7c9770ce01505b15a8f72980
Miguel Angel <xdadevelopers13@rocketmail.com>
2014-08-24 14:45:38 -0500

    Fixed 8610

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ ifneq ($(filter msm8974 msm8x74,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8974 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
 else
-ifneq ($(filter msm8226,$(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter msm8226 msm8610,$(TARGET_BOARD_PLATFORM)),)
     #This is for 8226 based platforms
     include $(call all-named-subdir-makefiles,msm8974)
 else
